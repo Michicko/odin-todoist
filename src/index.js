6d59528fe57f059ea44c9ae848c233dd3fd0d0fd
Michicko <53019881+Michicko@users.noreply.github.com>
2024-09-22 04:56:07 +0100

    styled task form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-import "./styles.css"
-console.log('Welcome to Todoist');
+import "./styles.css";
+
+const taskDialogBtn = document.querySelector(".add-task-btn");
+const taskDialog = document.querySelector("#task-dialog");
+const closeDialogBtn = document.querySelector("#close-dialog-btn");
+
+const openTaskDialog = () => {
+  taskDialog.showModal();
+};
+
+const closeTaskDialog = () => {
+  taskDialog.close();
+};
+
+taskDialog.showModal();
+
+taskDialogBtn.addEventListener("click", openTaskDialog);
+closeDialogBtn.addEventListener('click', closeTaskDialog);
